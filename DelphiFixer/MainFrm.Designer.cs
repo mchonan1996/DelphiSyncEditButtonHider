aba3aa6aecc201873844b6d6d09e1bd9cb53725d
Michael Honan <mc.honan1996@gmail.com>
2017-09-14 12:48:08 +1000

    Add project files.

---
+++ DelphiFixer/MainFrm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DelphiFixer {
+    partial class MainFrm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.FixerEnabledCheckBx = new System.Windows.Forms.CheckBox();
+            this.IntervalTextBx = new System.Windows.Forms.TextBox();
+            this.SaveIntervalBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HiderTmr = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(774, 176);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // FixerEnabledCheckBx
+            // 
+            this.FixerEnabledCheckBx.AutoSize = true;
+            this.FixerEnabledCheckBx.Location = new System.Drawing.Point(12, 12);
+            this.FixerEnabledCheckBx.Name = "FixerEnabledCheckBx";
+            this.FixerEnabledCheckBx.Size = new System.Drawing.Size(93, 17);
+            this.FixerEnabledCheckBx.TabIndex = 2;
+            this.FixerEnabledCheckBx.Text = "Hider Enabled";
+            this.FixerEnabledCheckBx.UseVisualStyleBackColor = true;
+            this.FixerEnabledCheckBx.CheckedChanged += new System.EventHandler(this.FixerEnabledCheckBx_CheckedChanged);
+            // 
+            // IntervalTextBx
+            // 
+            this.IntervalTextBx.Location = new System.Drawing.Point(517, 10);
+            this.IntervalTextBx.Name = "IntervalTextBx";
+            this.IntervalTextBx.Size = new System.Drawing.Size(100, 20);
+            this.IntervalTextBx.TabIndex = 3;
+            // 
+            // SaveIntervalBtn
+            // 
+            this.SaveIntervalBtn.Location = new System.Drawing.Point(699, 8);
+            this.SaveIntervalBtn.Name = "SaveIntervalBtn";
+            this.SaveIntervalBtn.Size = new System.Drawing.Size(87, 23);
+            this.SaveIntervalBtn.TabIndex = 4;
+            this.SaveIntervalBtn.Text = "Save Interval";
+            this.SaveIntervalBtn.UseVisualStyleBackColor = true;
+            this.SaveIntervalBtn.Click += new System.EventHandler(this.SaveIntervalBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Check every";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(617, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "milliseconds";
+            // 
+            // HiderTmr
+            // 
+            this.HiderTmr.Tick += new System.EventHandler(this.HiderTmr_Tick);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 225);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveIntervalBtn);
+            this.Controls.Add(this.IntervalTextBx);
+            this.Controls.Add(this.FixerEnabledCheckBx);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainFrm";
+            this.Text = "Delphi SyncEdit Button Hider";
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox FixerEnabledCheckBx;
+        private System.Windows.Forms.TextBox IntervalTextBx;
+        private System.Windows.Forms.Button SaveIntervalBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer HiderTmr;
+    }
+}
+
---
+++ DelphiFixer/MainFrm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DelphiFixer {
+    public partial class MainFrm : Form {
+
+        public delegate bool Win32Callback(IntPtr hwnd, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int FindWindowEx(int hwndParent, int hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindowAsync(int hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        static extern int GetForegroundWindow();
+
+        private const int SW_HIDE = 0;
+
+        public MainFrm() {
+            InitializeComponent();
+        }
+
+        private void DoHideSyncButton() {
+            var hwnd = FindWindow("TAppBuilder", null);
+
+            if (hwnd == 0) {
+                // Delphi not open.
+                return;
+            }
+            var activeWindow = GetForegroundWindow();
+            if (hwnd != activeWindow) {
+                // Delphi isn't active, no point wasting resources.
+                return;
+            }
+
+            var level1 = FindWindowEx(hwnd, 0, "TEditorDockPanel", null);
+            var level2 = FindWindowEx(level1, 0, "TEditWindow", null);
+            var level3 = FindWindowEx(level2, 0, "TPanel", null);
+            var level4 = FindWindowEx(level3, 0, "TPanel", null);
+
+            var level5Children = GetAllChildrenHandles(level4, "TPanel");
+            var level5 = level5Children[1];
+
+            // Second panel in the child5List holds the 6th child.
+            var level6 = FindWindowEx(level5, 0, "TPanel", null);
+
+            var level7Children = GetAllChildrenHandles(level6, "TPanel");
+            if (level7Children.Count < 1) {
+                richTextBox1.AppendText("Failed to find level 7 child (level 7 children list was 0).\r\n");
+                return;
+            }
+            var level7 = level7Children[0];
+
+            // Second panel in the child7List holds the 8th child.
+            var level8 = FindWindowEx(level7, 0, "TPanel", null);
+
+            var level9Children = GetAllChildrenHandles(level8, null);
+            if (level9Children.Count < 1) {
+                richTextBox1.AppendText("Failed to find level 9 child (level 9 children list was 0).\r\n");
+                return;
+            }
+            var level9 = level9Children[0];
+            var level10 = FindWindowEx(level9, 0, "TSyncButton", null);
+            ShowWindowAsync(level10, SW_HIDE);
+        }
+
+        private List<int> GetAllChildrenHandles(int hParent, string className) {
+            List<int> list = new List<int>();
+            int prevChild = 0;
+            int currChild = 0;
+
+            do {
+                currChild = FindWindowEx(hParent, prevChild, className, null);
+                if (currChild == 0)
+                    break;
+                list.Add(currChild);
+                prevChild = currChild;;
+            }
+            while (true);
+
+            return list;
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e) {
+            IntervalTextBx.Text = Properties.Settings.Default.interval.ToString();
+        }
+
+        private void FixerEnabledCheckBx_CheckedChanged(object sender, EventArgs e) {
+            if (FixerEnabledCheckBx.Checked) {
+                int intInterval;
+                if (int.TryParse(IntervalTextBx.Text, out intInterval)) {
+                    HiderTmr.Interval = intInterval;
+                } else {
+                    MessageBox.Show("Could not parse the interval textbox. It needs to contain a whole number in milliseconds.");
+                    FixerEnabledCheckBx.Checked = false;
+                    return;
+                }                
+            }
+            HiderTmr.Enabled = FixerEnabledCheckBx.Checked;
+        }
+
+        private void SaveIntervalBtn_Click(object sender, EventArgs e) {
+            int intInterval;
+            if (int.TryParse(IntervalTextBx.Text, out intInterval)) {
+                Properties.Settings.Default.interval = intInterval;
+                Properties.Settings.Default.Save();
+                MessageBox.Show("Saved!");
+            } else {
+                MessageBox.Show("Could not parse the interval textbox. It needs to contain a whole number in milliseconds.");
+                return;
+            }
+        }
+
+        private void HiderTmr_Tick(object sender, EventArgs e) {
+            HiderTmr.Enabled = false;
+            DoHideSyncButton();
+            if (FixerEnabledCheckBx.Checked) {
+                HiderTmr.Enabled = true;
+            }
+        }
+    }
+}
